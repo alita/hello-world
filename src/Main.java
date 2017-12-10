4718ce8847b25590a25eaba365611f428aaa22c1
Alit Atmaja <alitatmaja@gmail.com>
2017-12-10 11:02:42 +0900

    This commit is created from Intellij IDE

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void  main(String[] args) {
+        System.out.println("Hello World! Java ROCKS!!!");
+    }
+}
